a18582a96ef471093f21a6548364b4cb96511c21
rmrfalll <55176102+rmrfalll@users.noreply.github.com>
2019-09-11 17:54:43 +0800

    Update miku.sh

--- miku.sh
+++ miku.sh
@@ -22,7 +22,7 @@ fi
 while true
 do
 echo -e "\033[32m欢迎使用多功能脚本,请输入序号选择功能\033[0m"
-echo -e "\033[32m当前版本为:0.20\033[0m" 
+echo -e "\033[32m当前版本为:0.21\033[0m" 
 echo 1.tar备份恢复系统
 echo 2.科学上网脚本集合
 echo 3.各种工具脚本集合
@@ -60,9 +60,9 @@ then
 		fi
 	elif	[ "$m2" == 3 ]
 	then	
-		if [ ! -d  "/back" ]
+		if [ ! -d "/back"  ]
 		then
-  			mkdir /back
+ 			 mkdir /back
 		fi
 		read -p "请输入云端备份压缩包下载链接>>" m3
 		wget -c -N   $m3 -O backup.tgz

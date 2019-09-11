e879939ba12c0e976808a2b6fbffba4269bf0ee4
rmrfalll <55176102+rmrfalll@users.noreply.github.com>
2019-09-11 21:05:08 +0800

    Update miku_up.sh

--- miku_up.sh
+++ miku_up.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 rm -rf /root/miku.sh
 rm -rf /usr/local/sbin/miku
 wget -N https://raw.githubusercontent.com/rmrfalll/miku/master/miku.sh
 chmod -R 0777 /root/miku.sh 
 echo -e "\033[32m更新完成\033[0m"
-miku
+bash miku.sh
